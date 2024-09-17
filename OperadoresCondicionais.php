b3feabf784e501fbb37d26bc91ed2f66c06710b2
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-09-17 15:20:50 -0300

    Operadores de comparação/ condicionais

---
+++ OperadoresCondicionais.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    /*
+        Operadores de comparação (condicionais)
+    Igual (==) - verifica se os valores comparados são iguais
+    Idêntico (===) - verifica se os valores comparados são iguais e do mesmo tipo
+    Diferente (!=) - verifica se os valores comparados são diferentes
+    Diferente (<>) - verifica se os valores comparados são diferentes
+    Não idêntico (!==) - verifica se os valores comparados são diferentes e de tipos diferentes
+    Menor (<) - verifica se o valor da esquerda é menor que o valor da direita
+    Maior (>) - verifica se o valor da esquerda é maior que o valor da direita
+    Menor igual (<=) - verifica se o valor da esquerda é menor ou igual ao da direita
+    Maior igual (>=) - verifica se o valor da esquerda é maior ou igual ao da direita
+    
+    */
+    ?>
+</body>
+</html>
