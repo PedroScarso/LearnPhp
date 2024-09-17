39d33472fa7b6053f9d9aa7e03f43834cef65b25
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-09-17 08:37:29 -0300

    Adicionando arquivos de comentários em php

---
+++ Comentarios.php
@@ -0,0 +1,12 @@
+<?php
+    #  Comentário tipo 1 
+    // Comentário tipo 2
+    /* Comentário tipo 3 */
+
+
+    echo "Existem três tipos de comentários, são eles: <br>
+    # comentário <br>
+    // comentário <br>
+    ou  <br>
+    /*comentário*/ <br>"; 
+?>
