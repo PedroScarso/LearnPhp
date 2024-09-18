ccf6ca658227d6cef8b61a99cd395a1c2255a425
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-09-18 08:58:43 -0300

    Exemplos de if else com operadores condicionais

---
+++ IfElseOperadoresCondicionais.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if (1 == 0) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+        
+        if (1 === "1") {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 != 2) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 <> "1") {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 !== "1") {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 < 2) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 > 2) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 <= 1) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+
+        if (1 >= 1) {
+            echo "Palmeiras não tem mundial!<br><hr>";
+        } else {
+            echo "Pelo Corinthians com muito amor, até o fim!<br><hr>";
+        }
+    ?>
+</body>
+</html>
