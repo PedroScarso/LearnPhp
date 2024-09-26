cbdee900c93c381f4a024a5f19d945e1f62116e4
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-09-26 11:25:06 -0300

    escopo de variáveis aritméticas

---
+++ OperadoresAritmeticos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*
+            Operadores aritméticos
+            Adição (+) - Soma de valores
+            Subtração (-) - Diferença entre valores
+            Multiplicação (*) - Produto entre valores 
+            Divisão (/) - Quociente entre valores
+            Módulo (%) - Resto existente em uma operação de divisão
+        */ 
+    ?>
+</body>
+</html>
