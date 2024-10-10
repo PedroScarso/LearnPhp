7084369c4b36e44d7fb187805e93efdd457400a6
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-10-10 14:59:11 -0300

    Array Multidimensional

---
+++ ArrayMultidimensional.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Array multidimensional
+
+    $listaCoisas = ['frutas', 'cores'];
+
+    $listaCoisas['cores'] = ['azul','branco', 'cinza', 'dourado']; //ou array()
+
+    $listaCoisas['frutas'] = ['banana','maça', 'morango', 'Uva']; //ou array();
+
+    
+
+    // Array multidimensional com var_dump
+    // Esse array nada mais é que um array dentro de outro array Ou uma lista dentro de outra lista, existe a lista de coisas =(lista de listas), sendo assim lista de coisas conteria dentro de si uma lista de frutas, uma lista de roupas, uma lista de compras, uma lista de qualquer coisa. Assim como uma banana pode ser uma lista, já que possui características, então uma banana é uma lista de características, sendo, cor = amarelo, peso = 100 gramas e por aí vai.
+
+    echo 'Array multidimensional com var_dump<br>';
+    echo '<pre>';
+    var_dump($listaCoisas);
+    echo '</pre><hr>';
+    
+    
+    // Array multidimensional com print_r
+
+    
+    echo 'Array multidimensional (Lista Coisas) com print_r<br>';
+    echo '<pre>';
+    print_r($listaCoisas);
+    echo '</pre><hr>';
+    ?>
+</body>
+</html>
