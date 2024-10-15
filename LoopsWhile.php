9d4c12e0868d0150f638a331ee3382f678f0abc2
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-10-15 14:55:26 -0300

    While + instrução continue para criar exceção na regra

--- LoopsWhile.php
+++ LoopsWhile.php
@@ -49,7 +49,24 @@
             echo '<hr>';
        
 
+        // Acréscimo de 1 em 1 até 20, descartando 2, 6 e 15, usando continue para finalizar até o 20.
+        echo '<b>Loop com while e continue</b><br>';
+        echo '-- Início do loop --<br>';
+        $num3 = 1;
+        while($num3 < 20) {
+            
+            $num3++;
+           // Cada parâmetro passado no if segnifica uma parada da instrução, para fazer com que ele seja só uma exceção é realizado uso do CONTINUE, assim ele pra a instrução, porém continua e o critério de parada é o $num3 < 20, o CONTINUE não tem efeito sobre ele.
+           if($num3 == 2 || $num3 == 6 || $num3 == 15) {
+            continue;
+         }   
         
+        echo "$num3 . <br>";
+    
+         
+        }
+         echo '-- Fim do loop --';
+         echo '<hr>';
          
     ?>
 </body>
