849ff7ef6c15f2459eb4bb299b51c16fc378291b
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-10-15 16:32:03 -0300

    Estrutura de repetição For

---
+++ LoopsFor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*  FOR - Sintaxe
+            for (variável, condição, incremento) {
+            
+            }
+
+
+        */
+
+        for ($x = 1; $x <= 10; $x++) {
+            echo "$x . <br>";
+        }
+    ?>
+</body>
+</html>
