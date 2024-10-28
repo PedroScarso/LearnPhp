aec5a063251bea632114ef6012bd45c922fe98b6
Pedro PS. Scarso <pscarso@ALCENG.alcsp>
2024-10-28 10:17:43 -0300

    Array criado com credenciais, usuário setado inicialmente como false, condição de verificação se existe valor em POST email e senha, se sim, se os índices email e senha são iguais ao do array (futuramente será do BD) se sim, usuário autenticado, se usuário autenticado, redireciona para home, se não para index.php, falta colocar o aviso de usuário inválido

--- HelpDesk/valida_login.php
+++ HelpDesk/valida_login.php
@@ -1,10 +1,31 @@
 <?php
-    
-    $usuarios_app = array(
-        array('email' => 'adm@alcon.com.br', 'senha' => '1234'),
-        array('email' => 'user@alcon.com.br', 'senha' => '4321'),
-    );
-    echo '<pre>';
-    print_r ($usuarios_app);
-    echo '</pre>';
+ $usuarios_app = [
+    'email' => 'adm@alcon.com.br',
+    'senha' => '1234'
+];
+
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+print_r($usuarios_app['email']);
+print_r($usuarios_app['senha']);
+
+// Inicializa a variável de autenticação
+$usuario_autenticado = false;
+
+//  Condição verifica se existe o POST
+//  Se sim, verifica se o POST tem os índices email e senha, e se são iguais aos que estão no array $usuarios_app, se sim Usuário autenticado, Se usuário é autenticado redireciona para home, se não para index.php
+if (isset($_POST['email']) && isset($_POST['senha'])) {
+    if ($usuarios_app['email'] == $_POST['email'] && $usuarios_app['senha'] == $_POST['senha']) {
+        $usuario_autenticado = true;
+    }
+}
+
+// Verifica o status de autenticação
+if ($usuario_autenticado) {
+    header('Location: home.php');
+} else {
+    header('Location: index.php');
+}
+
 ?>
